c261e1d64d83720c1584a28243827e2e02b6e4d8
nizammarwadi <nizammarwadi94@gmail.com>
2021-06-24 19:43:36 +0700

    push all

--- components/atoms/ButtonResponse.vue
+++ components/atoms/ButtonResponse.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="response">
-        <p>Chat hami da</p>
+        <!-- <p>Layanan Cepat</p> -->
         <div class="image">
             <a href="https://wa.wizard.id/4a2aed" target="_blank">
             <img src="~/assets/images/whatsapp.png" class="rounded-circle" alt="whatsapp"></a>
@@ -23,14 +23,14 @@ export default {
         align-items: center;
         padding: 20px 20px;
     }
-    .response p {
+    /* .response p {
         font-weight: 700;
         color: white;
         background-color: rgb(0, 123, 255, 0.7);
         padding: 10px;
         margin: 0;
         border-radius: 5px;
-    }
+    } */
     .image {
         padding: 0px 10px;
     }
